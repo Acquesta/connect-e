d899c3277a27a29c2da4704f1b0d535d742a3eb5
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2024-09-17 12:30:57 -0300

    Add files via upload

--- src/components/noticias/Card.jsx
+++ src/components/noticias/Card.jsx
@@ -1,10 +1,25 @@
-function Card() {
+import { useState } from "react"
+
+function Card({ data, descricao, target = false }) {
+
+  const [progressBar, setProgressBar] = useState('')
+
+  let bgFundo = 'branco'
+  let corText = 'azulMahindra'
+  let bar = 'hidden'
+
+  if(target){
+    bgFundo = 'azulMahindra' 
+    corText = 'branco'
+    bar = 'block'
+  }
+
   return (
-    <div className=" bg-azulMahindra bg-opacity-75 w-1/4 rounded-xl text-branco">
-      <div className="h-2 w-full bg-branco rounded-t-xl"></div>
+    <div className={`bg-${bgFundo} bg-opacity-75 w-1/4 rounded-xl text-${corText} shadow-xl`}>
+      <div className={`${bar} h-2 w-[${progressBar}] bg-branco rounded-t-xl`}></div>
       <div className="px-6 pb-6">
-        <p className="mt-3 text-right">21 de setembro 2024</p>
-        <h4 className="text-xl mt-1">A criação de um circuito de Fórmula E</h4>
+        <p className="mt-3 text-right">{data}</p>
+        <h4 className="text-xl font-medium mt-1">{descricao}</h4>
       </div>
     </div>
   );
--- src/components/noticias/CardsNoticias.jsx
+++ src/components/noticias/CardsNoticias.jsx
@@ -2,11 +2,25 @@ import Card from "./Card";
 
 function CardsNoticias() {
     return ( 
-        <div className="mx-8 mt-[-30px] flex gap-4">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+        <div className="mx-8 mt-[-70px] flex gap-4">
+            <Card 
+                data='24 março 2023'
+                descricao='A criação de um circuito de Fórmula E'
+                target
+            />
+            <Card 
+                data='24 março 2023'
+                descricao='A criação de um circuito de Fórmula E'
+            />
+            <Card 
+                data='24 março 2023'
+                descricao='A criação de um circuito de Fórmula E'
+            />
+            <Card 
+                data='24 março 2023'
+                descricao='A criação de um circuito de Fórmula E'
+            />
+
         </div>
      );
 }
