edc5e2acec221582f29ed0f6efa28b1d146a08a3
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2024-09-17 08:37:56 -0300

    Add files via upload

--- src/components/sectionProjeto/card.jsx
+++ src/components/sectionProjeto/card.jsx
@@ -2,8 +2,8 @@ function Card({ img, title, description }) {
     return ( 
         <div className="border-2 border-azulMahindra rounded-2xl p-3">
             <img src={img} alt="" className="rounded-xl" />
-            <h4>{title}</h4>
-            <p>{description}</p>
+            <h4 className="text-2xl font-bold text-center text-azulMahindra my-5">{title}</h4>
+            <p className="mx-5 text-center">{description}</p>
         </div>
      );
 }
--- src/components/sectionProjeto/index.jsx
+++ src/components/sectionProjeto/index.jsx
@@ -1,6 +1,24 @@
 import Card from "./card";
 import solucaoImg from '../../assets/image/solucaoImg.png'
 
+const cardSolucao = [
+    {
+        img: solucaoImg,
+        title: 'Afinal, o que vamos solucionar?',
+        description: 'Todos têm conhecimento sobre corridas de carros tradicionais, movidos a combustão. Mas o principal ponto é: nem todos sabem o que é a Fórmula E. O desenvolvimento de carros elétricos está aumentando e, com isso, queremos que a popularização siga o mesmo caminho de ascensão.'
+    },
+    {
+        img: solucaoImg,
+        title: 'Nossa solução',
+        description: 'Consiste em quizzes com perguntas relacionadas a Fórmula E, carros e os cenários gerais, com rankings de pontuações com outras pessoas e premiações.'
+    },
+    {
+        img: solucaoImg,
+        title: 'Por que faremos isso?',
+        description: 'A gamificação é atrativa para todos os públicos por seu dinamismo. Pensando nisso, estimular as pessoas a se entreterem com quizzes, enquanto adquirem conhecimento sobre a Fórmula E, disputam rankings e ganham pontos e prêmios, pode despertar a curiosidade do público em geral.'
+    },
+]
+
 function SectionProjeto() {
     return ( 
         <div className="mx-8">
@@ -8,8 +26,8 @@ function SectionProjeto() {
             <div className="flex justify-between items-center gap-5">
                 <Card
                     img={solucaoImg}
-                    title={'Afinal, o que vamos solucionar?'}
-                    description={'Todos têm conhecimento sobre corridas de carros tradicionais, movidos a combustão. Mas o principal ponto é: nem todos sabem o que é a Fórmula E. O desenvolvimento de carros elétricos está aumentando e, com isso, queremos que a popularização siga o mesmo caminho de ascensão.'}
+                    title={''}
+                    description={''}
                 />
                 <Card
                     img={solucaoImg}
