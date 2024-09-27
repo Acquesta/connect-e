ca839787f88a6f4a98484d9cdcb6cfba4ed27a38
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2024-09-27 12:28:59 -0300

    Add files via upload

--- src/components/comunidade/comunidadeTop/ComunidadeAmigos.jsx
+++ src/components/comunidade/comunidadeTop/ComunidadeAmigos.jsx
@@ -13,31 +13,38 @@ function ComunidadeAmigos({ show, pedidos, aceitaPedido }) {
         })
     })
 
+    console.log(pedidos);
+
     return (
         <div className={`${show}`}>
                 <h4 className="text-azulMahindra font-semibold text-[1.5vw]">Solicitações</h4>
                 {
-                    pedidosUsuarios.map(usuario => (
-                        
-                        <div className="flex justify-between" key={usuario.id}>
-                            <p>{usuario.nome}</p>
-                            <div className="flex justify-between gap-1">
+                    pedidos != [] ? (
 
-                                <div onClick={() => aceitaPedido(usuario.id)} className="w-5 h-5 border-[1px] border-[#A9B7CC] rounded-[4px] flex justify-center items-center cursor-pointer">
-                                    <svg className="w-4 h-4 size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                                        stroke="green">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                                    </svg>
-                                </div>
-                                <div className="w-5 h-5 border-[1px] border-[#A9B7CC] rounded-[4px] flex justify-center items-center cursor-pointer">
-                                    <svg className="w-4 h-4 size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="red" >
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                                    </svg>
-
+                        pedidosUsuarios.map(usuario => (
+                            
+                            <div className="flex justify-between" key={usuario.id}>
+                                <p>{usuario.nome}</p>
+                                <div className="flex justify-between gap-1">
+    
+                                    <div onClick={() => aceitaPedido(usuario.id)} className="w-5 h-5 border-[1px] border-[#A9B7CC] rounded-[4px] flex justify-center items-center cursor-pointer">
+                                        <svg className="w-4 h-4 size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                            stroke="green">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                        </svg>
+                                    </div>
+                                    <div className="w-5 h-5 border-[1px] border-[#A9B7CC] rounded-[4px] flex justify-center items-center cursor-pointer">
+                                        <svg className="w-4 h-4 size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="red" >
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                        </svg>
+    
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
+                        ))
+
+                    ) : <p>Nenhuma solicitação</p>
+
                 }
         </div>
     );
